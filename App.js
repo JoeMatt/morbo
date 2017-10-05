baa3406d72cc401be7226690dd4c651d5c2f8c68
Sophie Idromenos <techmsi@users.noreply.github.com>
2017-10-05 15:56:27 -0400

    App: Render basic app

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+// React
+import React, { Component } from "react";
+import { AppRegistry, Image, StyleSheet, View } from "react-native";
+// Styles
+import styles from './components/styles';
+// Toolkit
+import { Text } from "react-native-elements";
+// Components
+import Articles from './components/Articles';
+
+const ENDPOINT = `http://projects.hearstnp.com/open/api/v1/article.php?id=12247734,12247132,12247986&key=1temp-public-key-09`;
 
 export default class App extends Component {
-    render() {
-        let pic = {
-            uri: 'https://ugc.kn3.net/i/origin/http://img1.wikia.nocookie.net/__cb20130831213334/es.futurama/images/3/3f/Morboo.jpg'
-        };
-        return (
-            <View style={styles.container}>
-                <Image source={pic} style={{width: 300, height: 250}}/>
-                <Text>Hello little man. I WILL DESTROY YOU!</Text>
-            </View>
-        );
+  state = {
+    data: null
+  };
+
+  async componentDidMount() {
+    console.log("App loaded");
+    const res = await fetch(ENDPOINT);
+    const data = await res.json();
+    console.log("data ", data);
+
+    this.setState({ data });
+  }
+
+  render() {
+    const { data } = this.state;
+    if (!data) {
+      return (
+        <View style={styles.container}>
+          <Text h3>Loading...</Text>
+        </View>
+      );
     }
+
+    return (
+      <View style={styles.container}>
+        <Text h4>Articles.</Text>
+        <Articles {...data} { ...this.props }/>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
