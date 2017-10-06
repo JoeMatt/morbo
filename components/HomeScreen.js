ee181bad0f583fa76ee4cf8602f230ad1ad1982a
Sophie Idromenos <techmsi@users.noreply.github.com>
2017-10-06 14:55:21 -0400

    Create HomeScreen.js

---
+++ components/HomeScreen.js
@@ -0,0 +1,52 @@
+// React
+import React, { Component } from "react";
+import { AppRegistry, Image, StyleSheet, View, ScrollView } from "react-native";
+// Styles
+import styles from './components/styles';
+// Toolkit
+import { Text, Button } from "react-native-elements";
+// Components
+import Articles from './Articles';
+import { StackNavigator } from "react-navigation";
+
+const ENDPOINT = `http://projects.hearstnp.com/open/api/v1/article.php?id=12247734,12247132,12247986&key=1temp-public-key-09`;
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+
+  state = {
+    data: null
+  };
+
+  async componentDidMount() {
+    console.log("App loaded");
+    const res = await fetch(ENDPOINT);
+    const data = await res.json();
+    console.log("data ", data);
+
+    this.setState({ data });
+  }
+
+  render() {
+    const { navigate } = this.props.navigation; 
+    const { data } = this.state;
+    if (!data) {
+      return (
+        <View style={styles.container}>
+          <Text h3>Loading...</Text>
+        </View>
+      );
+    }
+    return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text h4>Hearst Newspapers</Text>
+        <Button onPress={ () => { navigate("NetTest"); }  } title="Load Net Test" color="#3399aa" />
+        <Articles {...data} { ...this.props }/>
+      </ScrollView>
+    </View>
+    );
+  }
+}
